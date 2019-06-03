dfdb8852dcc2e58996b3947c678e60cd6fbbba2e
Bruno <brunorodriguesa7x@gmail.com>
2019-06-03 11:15:43 -0300

    Concluindo os testes da parameter service

--- src/app/services/parameters/parameters.service.spec.ts
+++ src/app/services/parameters/parameters.service.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ParametersService } from './parameters.service';
+import { DetalhesProdutoComponentStub } from 'src/app/componentes/mock/detalhesProduto.component.stub';
 
 describe('ParametersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+
+  let service: ParametersService;
+  let stub: DetalhesProdutoComponentStub;
+
+  beforeEach(() => {
+    const testBed = TestBed.configureTestingModule({
+      providers: [ParametersService, DetalhesProdutoComponentStub]
+    });
+    service = testBed.get(ParametersService);
+    stub = testBed.get(DetalhesProdutoComponentStub);
+  });
 
   it('should be created', () => {
-    const service: ParametersService = TestBed.get(ParametersService);
     expect(service).toBeTruthy();
+  });
+
+  describe('Quando o metodo [getValueOfPropertiesJson] for chamado', () => {
+    let resultado: string;
+    beforeEach(() => {
+      resultado = service.getValueOfPropertiesJson('detalhe_url');
+    });
+    it('Então [getValueOfPropertiesJson] deve ser chamado com valor verdadeiro', () => {
+      expect(resultado).toEqual('http://api-int.grupodimedservices.com.br/tst/mostruario/v3/itens/detalhe');
+    });
   });
 });
